8b55692d18f6dd1be7e9e40e9e895cd3dcc869ed
snxx-lppxx <snmgreenx@gmail.com>
2021-08-01 22:45:22 +0500

    minor

--- setuper/setuper.v
+++ setuper/setuper.v
@@ -4,12 +4,12 @@ import os
 import sys
 
 pub fn setup(name string,
-          version string,
-          repo string,
-          license string,
-          author string,
-          email string,
-          description string
-          ) {
+             version string,
+             repo string,
+             license string,
+             author string,
+             email string,
+             description string
+             ) {
     setup_prj = os.mkdir('${name}')
 }
